8d18efb9219e70b8be2775449f384107ddba6c81
Vini98br <viniciuss10@hotmail.com>
2021-02-14 02:08:06 -0300

    feat: 💄 adding theme

---
+++ src/theme/index.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    primary: '#D9552E',
+    secondary: '#FFFFFF',
+    dark_gray: '#1E232A',
+    gray: '#76868E',
+    gray_2: '#88969D',
+    light_gray: '#F4F5F6', 
+  }
+}
+
+export type AppTheme = typeof theme;
